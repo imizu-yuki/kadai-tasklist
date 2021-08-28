773443a06e59141613d9a79f5cff8e0f85f25fcc
imizu.yuki <student0107dd@gmail.com>
2021-08-28 09:23:45 +0000

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,8 @@
 @section('content')
     @if (Auth::check())
         {{ Auth::user()->name }}
-    @else
         <h1>タスク一覧</h1>
-        @if (count($tasks) > 0)
+        @if (count($tasks ?? '') > 0)
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -15,7 +14,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($tasks as $task)
+                    @foreach ($tasks ?? '' as $task)
                     <tr>
                         {{-- タスク詳細ページへのリンク --}}
                         <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
@@ -25,7 +24,8 @@
                     @endforeach
                 </tbody>
             </table>
-        @endif    
+        @endif
+    @else
     
         <div class="center jumbotron">
             <div class="text-center">
