7b60bf8ba359675ad91e4968496cee4d96ed0497
imizu.yuki <student0107dd@gmail.com>
2021-08-28 08:02:34 +0000

    ボタン追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,30 @@
                 {{-- ユーザ登録ページへのリンク --}}
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
+            
+            <h1>タスク一覧</h1>
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                    <th>ステータス</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    {{-- タスク詳細ページへのリンク --}}
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+            
         </div>
     @endif
 @endsection
